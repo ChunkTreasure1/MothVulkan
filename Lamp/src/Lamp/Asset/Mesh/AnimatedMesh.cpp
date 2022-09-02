8ffab032a15c9db67dad9a3d147a405fcbff810d
Niklas "zzWeeZz" Jakobsen <42831999+zzWeeZz@users.noreply.github.com>
2022-09-02 15:09:32 +0200

    Animation mesh class fixed

---
+++ Lamp/src/Lamp/Asset/Mesh/AnimatedMesh.cpp
@@ -0,0 +1,33 @@
+#include "lppch.h"
+#include "lppch.h"
+#include "AnimatedMesh.h"
+
+#include "Lamp/Rendering/Buffer/VertexBuffer.h"
+#include "Lamp/Rendering/Buffer/IndexBuffer.h"
+namespace Lamp
+{
+	void AnimatedModel::Construct()
+	{
+		m_vertexBuffer = VertexBuffer::Create(m_vertices, sizeof(Vertex) * (uint32_t)m_vertices.size());
+		m_indexBuffer = IndexBuffer::Create(m_indices, (uint32_t)m_indices.size());
+
+		glm::vec3 minAABB = glm::vec3(std::numeric_limits<float>::max());
+		glm::vec3 maxAABB = glm::vec3(std::numeric_limits<float>::min());
+
+		for (const auto& vert : m_vertices)
+		{
+			minAABB.x = std::min(minAABB.x, vert.position.x);
+			minAABB.y = std::min(minAABB.y, vert.position.y);
+			minAABB.z = std::min(minAABB.z, vert.position.z);
+
+			maxAABB.x = std::max(maxAABB.x, vert.position.x);
+			maxAABB.y = std::max(maxAABB.y, vert.position.y);
+			maxAABB.z = std::max(maxAABB.z, vert.position.z);
+		}
+
+		glm::vec3 center = (maxAABB + minAABB) * 0.5f;
+		float radius = glm::length(minAABB - maxAABB);
+
+		m_boundingSphere = { center, radius };
+	}
+}
---
+++ Lamp/src/Lamp/Asset/Mesh/AnimatedMesh.h
@@ -0,0 +1,57 @@
+#pragma once
+#include "Lamp/Asset/Asset.h"
+#include "Lamp/Asset/Mesh/SubMesh.h"
+#include "Lamp/Asset/Mesh/MultiMaterial.h"
+
+#include "Lamp/Rendering/Vertex.h"
+#include "Lamp/Rendering/BoundingStructures.h"
+
+#include <vector>
+#include <unordered_map>
+
+
+namespace Lamp
+{
+	class VertexBuffer;
+	class IndexBuffer;
+	class Material;
+
+	class AnimatedModel : public Asset
+	{
+	public:
+		AnimatedModel() = default;
+
+		void Construct();
+
+		inline const std::vector<SubMesh>& GetSubMeshes() const { return m_subMeshes; }
+		inline const Ref<MultiMaterial>& GetMaterial() const { return m_material; }
+
+		inline const size_t GetVertexCount() const { return m_vertices.size(); }
+		inline const size_t GetIndexCount() const { return m_indices.size(); }
+		inline const BoundingSphere& GetBoundingSphere() const { return m_boundingSphere; }
+
+		inline const Ref<VertexBuffer>& GetVertexBuffer() const { return m_vertexBuffer; }
+		inline const Ref<IndexBuffer>& GetIndexBuffer() const { return m_indexBuffer; }
+
+		static AssetType GetStaticType() { return AssetType::Mesh; }
+		AssetType GetType() override { return GetStaticType(); }
+
+	private:
+		friend class FbxImporter;
+		friend class GLTFImporter;
+		friend class LPGFImporter;
+		friend class MeshCompiler;
+
+		std::vector<SubMesh> m_subMeshes;
+
+		Ref<MultiMaterial> m_material;
+
+		std::vector<Vertex> m_vertices;
+		std::vector<uint32_t> m_indices;
+
+		Ref<VertexBuffer> m_vertexBuffer;
+		Ref<IndexBuffer> m_indexBuffer;
+
+		BoundingSphere m_boundingSphere;
+	};
+}
